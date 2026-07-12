3eab810c253be8a7523b6244a1e45090ce6c074d
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-12 14:37:36 +0000

    Creating Target table with merging for ensuring data consistency

--- First Data Pipeline/pipeline.sql
+++ First Data Pipeline/pipeline.sql
@@ -41,3 +41,48 @@ purge = true;
 -- view the data that was loaded
 select * from ORDERS_STG;
 
+
+-- view the contents of the stage again (should be empty again because the file was purged after loading)
+list @ORDERS_STAGE;
+
+-- create the target table
+use database BAKERY_DB;
+use schema ORDERS;
+create or replace table CUSTOMER_ORDERS (
+  customer varchar,
+  order_date date,
+  delivery_date date,
+  baked_good_type varchar,
+  quantity number,
+  source_file_name varchar,
+  load_ts timestamp
+);
+
+
+-- merge data from the staging table into the target table
+-- Listing 2.2  
+-- the target table
+merge into CUSTOMER_ORDERS tgt
+-- the source table
+using ORDERS_STG as src 
+-- the columns that ensure uniqueness
+on src.customer = tgt.customer 
+  and src.delivery_date = tgt.delivery_date 
+  and src.baked_good_type = tgt.baked_good_type
+-- update the target table with the values from the source table
+when matched then 
+  update set tgt.quantity = src.quantity, 
+    tgt.source_file_name = src.source_file_name, 
+    tgt.load_ts = current_timestamp()
+-- insert new values from the source table into the target table
+when not matched then
+  insert (customer, order_date, delivery_date, baked_good_type, 
+    quantity, source_file_name, load_ts)
+  values(src.customer, src.order_date, src.delivery_date, 
+    src.baked_good_type, src.quantity, src.source_file_name,
+    current_timestamp());
+
+
+-- view data after merging
+select * from CUSTOMER_ORDERS order by delivery_date desc;
+
