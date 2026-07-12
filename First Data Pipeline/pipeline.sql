70d16fc087a993a861b507011271a5edc9e523b0
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-12 14:36:21 +0000

    Creating Staging Table

--- First Data Pipeline/pipeline.sql
+++ First Data Pipeline/pipeline.sql
@@ -9,3 +9,16 @@ use database BAKERY_DB;
 use schema ORDERS;
 create stage ORDERS_STAGE;
 
+-- create staging table
+use database BAKERY_DB;
+use schema ORDERS;
+create table ORDERS_STG (
+  customer varchar,
+  order_date date,
+  delivery_date date,
+  baked_good_type varchar,
+  quantity number,
+  source_file_name varchar,
+  load_ts timestamp
+);
+
