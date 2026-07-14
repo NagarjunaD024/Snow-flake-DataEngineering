0bb02f9cc186c8f2fafeafe34ae0141692c29216
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-14 13:20:37 +0000

    External stage using Credentials

--- Data Staging/Staging.sql
+++ Data Staging/Staging.sql
@@ -25,3 +25,22 @@ create database if not exists BAKERY_DB;
 use database BAKERY_DB;
 create schema EXTERNAL_ORDERS;
 use schema EXTERNAL_ORDERS;
+
+-- create an external stage using the storage integration
+-- Listing 3.1 
+create stage BISTRO_STAGE
+  storage_integration = BISTRO_INTEGRATION
+  url = 'azure://bakeryorders001.blob.core.windows.net/orderfiles';
+
+-- Upload a sample file named Orders_2023-08-04.csv to the storage container
+
+-- view files in the external stage
+list @BISTRO_STAGE;
+
+-- create an external stage using a SAS token
+create stage BISTRO_SAS_STAGE
+  URL = 'azure://bakeryorders001.blob.core.windows.net/orderfiles'
+  CREDENTIALS=(AZURE_SAS_TOKEN = '?sv=2023-...%3D'); --generate and use your own SAS token
+
+-- view files in the external stage
+list @BISTRO_SAS_STAGE;
