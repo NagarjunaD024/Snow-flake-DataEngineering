d133a853e208b509474b4b30c7651884be2c5981
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-18 17:32:04 +0000

    create a view that combines data from individual staging tables

--- Transforming data/Tranformingdata (Stored_procedures).sql
+++ Transforming data/Tranformingdata (Stored_procedures).sql
@@ -8,3 +8,17 @@ use schema TRANSFORM;
 
 
 
+-- create a view that combines data from individual staging tables
+create view ORDERS_COMBINED_STG as
+select customer, order_date, delivery_date, baked_good_type, quantity, source_file_name, load_ts
+from bakery_db.orders.ORDERS_STG
+union all
+select customer, order_date, delivery_date, baked_good_type, quantity, source_file_name, load_ts
+from bakery_db.external_orders.ORDERS_BISTRO_STG
+union all
+select customer, order_date, delivery_date, baked_good_type, quantity, source_file_name, load_ts
+from bakery_db.external_json_orders.ORDERS_PARK_INN_STG;
+
+
+
+
