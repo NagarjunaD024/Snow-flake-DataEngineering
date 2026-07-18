7fed4c004d7d71c094466508eb9802f19b673b8f
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-18 17:25:38 +0000

    create a new schema in the BAKERY_DB database for json Ingestion

--- Transforming data/Transformingdata(Storage_IntegrationS3).sql
+++ Transforming data/Transformingdata(Storage_IntegrationS3).sql
@@ -17,3 +17,13 @@ describe storage integration PARK_INN_INTEGRATION;
 
 -- grant usage on storage integration so that the SYSADMIN role can use it
 grant usage on integration PARK_INN_INTEGRATION to role SYSADMIN;
+
+
+-- create a new schema in the BAKERY_DB database for json Ingestion
+use role SYSADMIN;
+create warehouse if not exists BAKERY_WH with warehouse_size = 'XSMALL';
+use warehouse BAKERY_WH;
+create database if not exists BAKERY_DB;
+use database BAKERY_DB;
+create schema EXTERNAL_JSON_ORDERS;
+use schema EXTERNAL_JSON_ORDERS;
