224c4db097ad83c20b6b38c20cf143104106210c
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-18 17:38:40 +0000

    modify the stored procedure: add logging

--- Transforming data/Tranformingdata (Stored_procedureswitlogging).sql
+++ Transforming data/Tranformingdata (Stored_procedureswitlogging).sql
@@ -17,3 +17,35 @@ grant modify log level on account to role SYSADMIN;
 use role SYSADMIN;
 -- set the log level on the stored procedure to DEBUG
 alter procedure LOAD_CUSTOMER_ORDERS() set log_level = DEBUG;
+
+
+-- modify the stored procedure: add logging
+use database BAKERY_DB;
+use schema TRANSFORM;
+create or replace procedure LOAD_CUSTOMER_ORDERS()
+returns varchar
+language sql
+as
+$$
+begin
+  SYSTEM$LOG_DEBUG('LOAD_CUSTOMER_ORDERS begin ');
+  merge into CUSTOMER_ORDERS_COMBINED tgt
+using ORDERS_COMBINED_STG as src
+on src.customer = tgt.customer and src.delivery_date = tgt.delivery_date and src.baked_good_type = tgt.baked_good_type
+when matched then 
+  update set tgt.quantity = src.quantity, 
+    tgt.source_file_name = src.source_file_name, 
+    tgt.load_ts = current_timestamp()
+when not matched then
+  insert (customer, order_date, delivery_date, 
+    baked_good_type, quantity, source_file_name, load_ts)
+  values(src.customer, src.order_date, src.delivery_date,
+    src.baked_good_type, src.quantity, src.source_file_name,
+    current_timestamp());
+  return 'Load completed. ' || SQLROWCOUNT || ' rows affected.';
+exception
+  when other then
+    return 'Load failed with error message: ' || SQLERRM;
+end;
+$$
+;
