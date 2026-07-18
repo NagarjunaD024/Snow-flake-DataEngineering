67211c7ad3de46d2b97eb6ed9d460dac22e66ed2
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-18 17:24:36 +0000

    describe the storage integration

--- Transforming data/Transformingdata(Storage_IntegrationS3).sql
+++ Transforming data/Transformingdata(Storage_IntegrationS3).sql
@@ -9,3 +9,11 @@ create storage integration PARK_INN_INTEGRATION
   enabled = true
   storage_aws_role_arn = 'arn:aws:iam::123456789012:role/Snowflake-demo'
   storage_allowed_locations = ('s3://parkinnorders8977/');
+
+  -- describe the storage integration and take note of the following parameters:
+-- - STORAGE_AWS_IAM_USER_ARN
+-- - STORAGE_AWS_EXTERNAL_ID
+describe storage integration PARK_INN_INTEGRATION;
+
+-- grant usage on storage integration so that the SYSADMIN role can use it
+grant usage on integration PARK_INN_INTEGRATION to role SYSADMIN;
