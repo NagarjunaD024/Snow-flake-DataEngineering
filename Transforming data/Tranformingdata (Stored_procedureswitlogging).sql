ba4cc7d1e0b07b57bb1e788a89d99c2c500c1cfe
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-18 17:37:56 +0000

    associating the event table with the account

--- Transforming data/Tranformingdata (Stored_procedureswitlogging).sql
+++ Transforming data/Tranformingdata (Stored_procedureswitlogging).sql
@@ -5,3 +5,10 @@ use schema TRANSFORM;
 use role SYSADMIN;
 -- create an event table
 create event table BAKERY_EVENTS;
+
+
+use role ACCOUNTADMIN;
+-- associate the event table with the account
+alter account set event_table = BAKERY_DB.TRANSFORM.BAKERY_EVENTS;
+-- grant privileges to set log level to the SYSADMIN role
+grant modify log level on account to role SYSADMIN;
