1634d3e2f330c9787f912ba3be4ed7a7281d9ed2
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-18 17:31:49 +0000

    create schema TRANSFORM for consolidation

---
+++ Transforming data/Tranformingdata (Stored_procedures).sql
@@ -0,0 +1,10 @@
+use role SYSADMIN;
+create warehouse if not exists BAKERY_WH with warehouse_size = 'XSMALL';
+use warehouse BAKERY_WH;
+create database if not exists BAKERY_DB;
+use database BAKERY_DB;
+create schema TRANSFORM;
+use schema TRANSFORM;
+
+
+
