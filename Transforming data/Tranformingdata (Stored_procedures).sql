7d6ff1c3a495c56bd9cc33e660302e725817bcdb
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-18 17:34:47 +0000

    modify the stored procedure with exception handling

--- Transforming data/Tranformingdata (Stored_procedures).sql
+++ Transforming data/Tranformingdata (Stored_procedures).sql
@@ -112,3 +112,37 @@ $$
 
 -- execute the stored procedure
 call LOAD_CUSTOMER_ORDERS();
+
+
+
+
+-- modify the stored procedure: add exception handling
+
+use database BAKERY_DB;
+use schema TRANSFORM;
+create or replace procedure LOAD_CUSTOMER_ORDERS()
+returns varchar
+language sql
+as
+$$
+begin
+  merge into CUSTOMER_ORDERS_COMBINED tgt
+using ORDERS_COMBINED_STG as src
+on src.customer = tgt.customer and src.delivery_date = tgt.delivery_date and src.baked_good_type = tgt.baked_good_type
+when matched then 
+  update set tgt.quantity = src.quantity, 
+    tgt.source_file_name = src.source_file_name, 
+    tgt.load_ts = current_timestamp()
+when not matched then
+  insert (customer, order_date, delivery_date, 
+    baked_good_type, quantity, source_file_name, load_ts)
+  values(src.customer, src.order_date, src.delivery_date,
+    src.baked_good_type, src.quantity, src.source_file_name,
+    current_timestamp());
+  return 'Load completed. ' || SQLROWCOUNT || ' rows affected.';
+exception
+  when other then
+    return 'Load failed with error message: ' || SQLERRM;
+end;
+$$
+;
