e40874f400e9f67ba50380696c386df4f50623f5
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-19 20:06:13 +0000

    Describing and granting Usage

--- Continuous data ingestion/ContinousIngestion.sql
+++ Continuous data ingestion/ContinousIngestion.sql
@@ -10,3 +10,13 @@ create storage integration SPEEDY_INTEGRATION
   azure_tenant_id = '5f3419cf-49eb-4713-9edb-8a3c39a7989c'
   storage_allowed_locations = 
     ('azure://bakeryorders897764.blob.core.windows.net/speedyservicefiles8977/');
+
+
+
+-- describe the storage integration and take note of the following parameters:
+-- - AZURE_CONSENT_URL
+-- - AZURE_MULTI_TENANT_APP_NAME
+describe integration SPEEDY_INTEGRATION;
+
+-- grant usage on storage integration so that the SYSADMIN role can use it
+grant usage on integration SPEEDY_INTEGRATION to role SYSADMIN;
