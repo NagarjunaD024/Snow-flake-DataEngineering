0ef2e84a72e3f2c52f7d86f232c3a6b0c1273110
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-21 07:40:17 +0000

    Import packages

---
+++ Executing code on IDE with Snowpark/Snowpark_ingest_CSV.py
@@ -0,0 +1,7 @@
+# import Session from the snowflake.snowpark package
+from snowflake.snowpark import Session
+# import data types from the snowflake.snowpark package
+from snowflake.snowpark.types import StructType, StructField, DateType, StringType, DecimalType
+# import json package for reading connection parameters
+import json
+
