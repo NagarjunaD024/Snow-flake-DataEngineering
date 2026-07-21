312b95ac4b790acd64fa82c40f89d01f21920d23
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-21 07:48:47 +0000

    establish a connection with Snowflake.

--- Executing code on IDE with Snowpark/Snowpark_ingest_CSV.py
+++ Executing code on IDE with Snowpark/Snowpark_ingest_CSV.py
@@ -9,3 +9,24 @@ import json
 source_file_name = 'Orders_2023-07-09.csv'
 
 
+# establish a connection with Snowflake
+
+#Refer to Listing 6.4
+# read the credentials from a file
+credentials = json.load(open('connection_parameters.json'))
+# create a dictionary with the connection parameters
+connection_parameters_dict = {
+    "account": credentials["account"],
+    "user": credentials["user"],
+    "password": credentials["password"],
+    "role": credentials["role"],
+    "warehouse": credentials["warehouse"],
+    "database": credentials["database"],
+    "schema": credentials["schema"]  # optional
+}  
+
+# create a session object for the Snowpark session
+my_session = Session.builder.configs(connection_parameters_dict).create()
+
+
+
