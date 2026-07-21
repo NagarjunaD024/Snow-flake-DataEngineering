bb6736ec2876ac95353a6a1905cb0c5d098780fc
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-21 06:21:52 +0000

    Script to establish connection

---
+++ Executing code on IDE with Snowpark/Snowpark_connection_params.py
@@ -0,0 +1,31 @@
+
+#Listing 6.4
+# import Session from the snowflake.snowpark package
+from snowflake.snowpark import Session
+
+import json
+
+# read the credentials from a file
+credentials = json.load(open('connection_parameters.json'))
+
+# create a dictionary with the connection parameters
+connection_parameters_dict = {
+    "account": credentials["account"],
+    "user": credentials["user"],
+    "password": credentials["password"],
+    "role": credentials["role"],
+    "warehouse": credentials["warehouse"],
+    "database": credentials["database"],
+    "schema": credentials["schema"]  # optional
+}  
+
+# create the session
+my_session = Session.builder.configs(connection_parameters_dict).create()
+
+# select the current timestamp
+ts = my_session.sql("select current_timestamp()").collect()
+# print the output to the console
+print(ts)
+
+# close the session
+my_session.close()
