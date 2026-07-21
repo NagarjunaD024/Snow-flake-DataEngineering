4c04124e7fb9285a13c49c2f1374d1af9f700e98
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-21 07:49:38 +0000

    channel the file into the stage

--- Executing code on IDE with Snowpark/Snowpark_ingest_CSV.py
+++ Executing code on IDE with Snowpark/Snowpark_ingest_CSV.py
@@ -29,4 +29,12 @@ connection_parameters_dict = {
 my_session = Session.builder.configs(connection_parameters_dict).create()
 
 
+# put the file into the stage
+result = my_session.file.put(source_file_name, "@orders_stage")
+print(result)
+
+
+
+
+
 
