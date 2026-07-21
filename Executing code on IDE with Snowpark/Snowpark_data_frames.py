5788690da3a309550d35aa563cd2eca86520d1c8
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-21 08:38:10 +0000

    establish a connection with Snowflake

--- Executing code on IDE with Snowpark/Snowpark_data_frames.py
+++ Executing code on IDE with Snowpark/Snowpark_data_frames.py
@@ -2,3 +2,22 @@
 from snowflake.snowpark import Session
 # import json package for reading connection parameters
 import json
+
+
+# establish a connection with Snowflake
+
+# read the credentials from a file
+credentials = json.load(open('connection_parameters.json'))
+# create a dictionary with the connection parameters
+connection_parameters_dict = {
+    "account": credentials["account"],
+    "user": credentials["user"],
+    "password": credentials["password"],
+    "role": credentials["role"],
+    "warehouse": credentials["warehouse"],
+    "database": credentials["database"],
+    "schema": credentials["schema"]  # optional
+}  
+
+# create a session object for the Snowpark session
+my_session = Session.builder.configs(connection_parameters_dict).create()
