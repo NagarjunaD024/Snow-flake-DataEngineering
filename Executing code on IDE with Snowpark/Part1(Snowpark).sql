6353f9cc1b973534495430a07d9034ea5451b389
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-21 08:45:04 +0000

    Sql commands to create SNOWPARK, ORDERS_STAGE

---
+++ Executing code on IDE with Snowpark/Part1(Snowpark).sql
@@ -0,0 +1,10 @@
+-- create a new schema in the BAKERY_DB database 
+use role SYSADMIN;
+create warehouse if not exists BAKERY_WH with warehouse_size = 'XSMALL';
+create database if not exists BAKERY_DB;
+use database BAKERY_DB;
+create schema SNOWPARK;
+use schema SNOWPARK;
+
+-- 	Ingesting Data from a CSV File into a Snowflake Table
+create stage ORDERS_STAGE;
