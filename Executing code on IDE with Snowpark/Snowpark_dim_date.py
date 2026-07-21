8113e4ed229cd037471159d712d451db3c1342d5
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-21 06:54:45 +0000

    establish a connection with Snowflake.

--- Executing code on IDE with Snowpark/Snowpark_dim_date.py
+++ Executing code on IDE with Snowpark/Snowpark_dim_date.py
@@ -38,3 +38,23 @@ holiday_flags = [[d, is_holiday(d, 'US')] for d in dates]
 
 # print the holiday_flags list of lists locally to check that the data is as expected
 print(holiday_flags)
+
+
+
+# establish a connection with Snowflake because we need the Snowpark API
+
+# read the credentials from a file
+credentials = json.load(open('connection_parameters.json'))
+# create a dictionary with the connection parameters
+connection_parameters_dict = {
+    "account": credentials["account"],
+    "user": credentials["user"],
+    "password": credentials["password"],
+    "role": credentials["role"],
+    "warehouse": credentials["warehouse"],
+    "database": credentials["database"],
+    "schema": credentials["schema"]  # optional
+}  
+
+# create a session object for the Snowpark session
+my_session = Session.builder.configs(connection_parameters_dict).create()
