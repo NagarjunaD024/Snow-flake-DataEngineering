398392ef3aadb9d636caa55b0d1b46a6bad0b54c
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-21 07:41:12 +0000

    Adding source_file_name variable

--- Executing code on IDE with Snowpark/Snowpark_ingest_CSV.py
+++ Executing code on IDE with Snowpark/Snowpark_ingest_CSV.py
@@ -5,3 +5,7 @@ from snowflake.snowpark.types import StructType, StructField, DateType, StringTy
 # import json package for reading connection parameters
 import json
 
+
+source_file_name = 'Orders_2023-07-09.csv'
+
+
