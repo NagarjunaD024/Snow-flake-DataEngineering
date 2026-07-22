0a67517835f02c743472c43d62eddff38a82e8e7
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-22 12:38:43 +0000

    use the SYSADMIN role

--- Augmenting data with outputs from LLMs/review_sentiment.sql
+++ Augmenting data with outputs from LLMs/review_sentiment.sql
@@ -1,3 +1,9 @@
 -- grant the SNOWFLAKE.CORTEX_USER to the SYSADMIN role
 use role ACCOUNTADMIN;
 grant database role SNOWFLAKE.CORTEX_USER to role SYSADMIN;
+
+
+-- use the SYSADMIN role and the REVIEWS schema in the BAKERY_DB database
+use role SYSADMIN;
+use database BAKERY_DB;
+use schema REVIEWS;
