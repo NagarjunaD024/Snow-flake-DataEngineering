ea1955d3e9ad7a2db1dd648d48f3d8fb4be475e0
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-22 12:23:47 +0000

    create a new schema

---
+++ Augmenting data with outputs from LLMs/customer_reviews.sql
@@ -0,0 +1,7 @@
+-- create a new schema in the BAKERY_DB database
+use role SYSADMIN;
+create warehouse if not exists BAKERY_WH with warehouse_size = 'XSMALL';
+create database if not exists BAKERY_DB;
+use database BAKERY_DB;
+create schema REVIEWS;
+use schema REVIEWS;
