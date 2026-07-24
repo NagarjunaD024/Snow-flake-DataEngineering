88665431f936764e011fad87d983b9a71544e4c1
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-24 12:18:10 +0000

    create a set of virtual warehouses

--- Controlling costs/Controlling.sql
+++ Controlling costs/Controlling.sql
@@ -4,3 +4,11 @@ use database BAKERY_DB;
 use schema RETAIL_ANALYSIS;
 
 
+
+-- create a set of virtual warehouses in increasing sizes
+create warehouse BAKERY_WH_XSMALL with warehouse_size = 'xsmall';
+create warehouse BAKERY_WH_SMALL with warehouse_size = 'small';
+create warehouse BAKERY_WH_MEDIUM with warehouse_size = 'medium';
+create warehouse BAKERY_WH_LARGE with warehouse_size = 'large';
+
+
