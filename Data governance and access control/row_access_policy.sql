5902b4bfb1314fb691610f6c378a71d7fecf6e08
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-25 22:09:02 +0000

    create the functional roles

--- Data governance and access control/row_access_policy.sql
+++ Data governance and access control/row_access_policy.sql
@@ -5,3 +5,9 @@ use database BAKERY_DB;
 -- create schema with managed access using the SYSADMIN role
 create schema DG with managed access;
 grant all on schema DG to role BAKERY_FULL;
+
+
+use role USERADMIN;
+-- create the functional roles
+create role DATA_ANALYST_BREAD;
+create role DATA_ANALYST_PASTRY;
