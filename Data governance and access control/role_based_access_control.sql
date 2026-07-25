7610436dd378e85c079827c5c98535b02828f7cf
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-25 22:03:29 +0000

    create the functional roles

--- Data governance and access control/role_based_access_control.sql
+++ Data governance and access control/role_based_access_control.sql
@@ -15,3 +15,8 @@ use role USERADMIN;
 -- create the access roles for full access and for read-only access
 create role BAKERY_FULL;
 create role BAKERY_READ;
+
+
+-- create the functional roles
+create role DATA_ENGINEER;
+create role DATA_ANALYST;
