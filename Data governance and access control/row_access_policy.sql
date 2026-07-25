01050ef74c7c164dd59a02ad2fc906dc29c31a0b
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-25 22:09:29 +0000

    grant the functional roles to the users who perform those business functions

--- Data governance and access control/row_access_policy.sql
+++ Data governance and access control/row_access_policy.sql
@@ -16,3 +16,11 @@ create role DATA_ANALYST_PASTRY;
 -- grant the BAKERY_READ access role to functional roles
 grant role BAKERY_READ to role DATA_ANALYST_BREAD;
 grant role BAKERY_READ to role DATA_ANALYST_PASTRY;
+
+
+-- grant the functional roles to the users who perform those business functions
+-- in this exercise we grant both functional roles to our current user to be able to test them
+
+set my_current_user = current_user();
+grant role DATA_ANALYST_BREAD to user IDENTIFIER($my_current_user);
+grant role DATA_ANALYST_PASTRY to user IDENTIFIER($my_current_user);
