c6ac444b5e7627ad88b0f238dead1aeb92f80e73
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-26 09:45:30 +0000

    grant full privileges on all schemas

--- Designing data pipelines/create_schema.sql
+++ Designing data pipelines/create_schema.sql
@@ -10,3 +10,10 @@ create schema MGMT with managed access;
 
 -- using the SECURITYADMIN role (because this role has the MANAGE GRANTS privilege)
 use role SECURITYADMIN;
+
+
+-- grant full privileges on all schemas to the BAKERY_FULL role
+grant all on schema BAKERY_DB.EXT to role BAKERY_FULL;
+grant all on schema BAKERY_DB.STG to role BAKERY_FULL;
+grant all on schema BAKERY_DB.DWH to role BAKERY_FULL;
+grant all on schema BAKERY_DB.MGMT to role BAKERY_FULL;
