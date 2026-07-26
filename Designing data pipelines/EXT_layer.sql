21c8554ac8bd2beb3e21626492b6407a6efd9ecb
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-26 09:46:59 +0000

    create the extract table for the orders in raw (json) format

--- Designing data pipelines/EXT_layer.sql
+++ Designing data pipelines/EXT_layer.sql
@@ -6,3 +6,14 @@ use role DATA_ENGINEER;
 use warehouse BAKERY_WH;
 use database BAKERY_DB;
 use schema EXT;
+
+
+-- view files in the stage
+list @JSON_ORDERS_STAGE;
+
+-- create the extract table for the orders in raw (json) format
+create table JSON_ORDERS_EXT (
+  customer_orders variant,
+  source_file_name varchar,
+  load_ts timestamp
+);
