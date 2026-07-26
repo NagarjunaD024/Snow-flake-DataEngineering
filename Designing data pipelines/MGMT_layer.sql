29dc6e5c83a6c24818163fede93dfb996249a52c
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-26 09:51:50 +0000

    create view in the MGMT schema

---
+++ Designing data pipelines/MGMT_layer.sql
@@ -0,0 +1,12 @@
+use role DATA_ENGINEER;
+use warehouse BAKERY_WH;
+use database BAKERY_DB;
+use schema MGMT;
+
+-- create view in the MGMT schema that summarizes orders by delivery date and baked good type, adding the baked good category
+create view ORDERS_SUMMARY as
+select ORD.delivery_date, PRD.product_name, PRD.category, sum(ORD.quantity) as total_quantity
+from dwh.ORDERS ORD
+left join dwh.PRODUCT PRD
+on ORD.product_id = PRD.product_id
+group by all;
