098c47e1692804391c3c20d0cd0c811ba4925799
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-26 09:45:02 +0000

    create schema with managed access

---
+++ Designing data pipelines/create_schema.sql
@@ -0,0 +1,8 @@
+use role SYSADMIN;
+use database BAKERY_DB;
+
+-- create schema with managed access using the SYSADMIN role
+create schema EXT with managed access;
+create schema STG with managed access;
+create schema DWH with managed access;
+create schema MGMT with managed access;
