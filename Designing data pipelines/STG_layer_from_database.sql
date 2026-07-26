4077bfdbd56796dde5af7028ce8477dc52b7ffc4
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-07-26 09:49:47 +0000

    create table PRODUCT

--- Designing data pipelines/STG_layer_from_database.sql
+++ Designing data pipelines/STG_layer_from_database.sql
@@ -28,3 +28,29 @@ insert into PARTNER values
 (112, 'Murphy Mill', '700 Darren Centers', 'A', '2023-06-01');
 
 select * from PARTNER;
+
+
+create table PRODUCT (
+product_id integer,
+product_name varchar,
+category varchar,
+min_quantity integer,
+price number(18,2),
+valid_from date
+);
+
+insert into PRODUCT values
+(1, 'Baguette', 'Bread', 2, 2.5, '2023-06-01'),
+(2, 'Bagel', 'Bread', 6, 1.3, '2023-06-01'), 
+(3, 'English Muffin', 'Bread', 6, 1.2, '2023-06-01'), 
+(4, 'Croissant', 'Pastry', 4, 2.1, '2023-06-01'), 
+(5, 'White Loaf', 'Bread', 1, 1.8, '2023-06-01'), 
+(6, 'Hamburger Bun', 'Bread', 10, 0.9, '2023-06-01'), 
+(7, 'Rye Loaf', 'Bread', 1, 3.2, '2023-06-01'), 
+(8, 'Whole Wheat Loaf', 'Bread', 1, 2.8, '2023-06-01'), 
+(9, 'Muffin', 'Pastry', 12, 3.0, '2023-06-01'), 
+(10, 'Cinnamon Bun', 'Pastry', 6, 3.4, '2023-06-01'), 
+(11, 'Blueberry Muffin', 'Pastry', 12, 3.6, '2023-06-01'), 
+(12, 'Chocolate Muffin', 'Pastry', 12, 3.6, '2023-06-01'); 
+
+select * from PRODUCT;
