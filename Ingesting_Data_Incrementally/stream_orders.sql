d997827e9fd94d6b6a3327a212d677dad5a63588
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-08-07 17:41:45 +0000

    insert the flattened data from the stream into the staging table

--- Ingesting_Data_Incrementally/stream_orders.sql
+++ Ingesting_Data_Incrementally/stream_orders.sql
@@ -52,3 +52,19 @@ create table STG.JSON_ORDERS_TBL_STG (
   source_file_name varchar,
   load_ts timestamp
 );
+
+
+
+-- insert the flattened data from the stream into the staging table
+insert into STG.JSON_ORDERS_TBL_STG
+select 
+  customer_orders:"Customer"::varchar as customer, 
+  customer_orders:"Order date"::date as order_date, 
+  CO.value:"Delivery date"::date as delivery_date,
+  DO.value:"Baked good type":: varchar as baked_good_type,
+  DO.value:"Quantity"::number as quantity,
+  source_file_name,
+  load_ts
+from EXT.JSON_ORDERS_STREAM,
+lateral flatten (input => customer_orders:"Orders") CO,
+lateral flatten (input => CO.value:"Orders by day") DO;
