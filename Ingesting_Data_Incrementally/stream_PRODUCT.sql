2f86aee42671efef660035870e1ae19d8f7d7d3c
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-08-07 17:43:50 +0000

    create a stream on the table in the staging layer

--- Ingesting_Data_Incrementally/stream_PRODUCT.sql
+++ Ingesting_Data_Incrementally/stream_PRODUCT.sql
@@ -6,3 +6,8 @@ use schema DWH;
 -- create a table in the data warehouse layer and populate initially with the data from the staging layer
 create table PRODUCT_TBL as select * from STG.PRODUCT;
 select * from PRODUCT_TBL;
+
+
+-- create a stream on the table in the staging layer
+use schema STG;
+create stream PRODUCT_STREAM on table PRODUCT;
