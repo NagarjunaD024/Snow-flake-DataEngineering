8f2f361812a1b384dd3e5472d5d179a014e4ce34
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-08-07 17:44:04 +0000

    make some changes in the staging table: one update and one insert

--- Ingesting_Data_Incrementally/stream_PRODUCT.sql
+++ Ingesting_Data_Incrementally/stream_PRODUCT.sql
@@ -11,3 +11,13 @@ select * from PRODUCT_TBL;
 -- create a stream on the table in the staging layer
 use schema STG;
 create stream PRODUCT_STREAM on table PRODUCT;
+
+
+
+-- make some changes in the staging table: one update and one insert
+update PRODUCT
+  set category = 'Pastry', valid_from = '2023-08-08'
+  where product_id = 3;
+  
+insert into PRODUCT values
+  (13, 'Sourdough Bread', 'Bread', 1, 3.6, '2023-08-08');
