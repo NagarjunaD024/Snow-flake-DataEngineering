5591eb7784d73d2d19ef3dbdf5df8d09274beb86
Nagarjuna Gottipati <64250120+NagarjunaD024@users.noreply.github.com>
2026-08-07 17:48:17 +0000

    create a dynamic table ORDERS_TBL in the DWH schema using the previous query

--- Ingesting_Data_Incrementally/dynamic_table.sql
+++ Ingesting_Data_Incrementally/dynamic_table.sql
@@ -15,3 +15,18 @@ inner join STG.PARTNER PT
   on PT.partner_name = ORD.customer
 inner join STG.PRODUCT PRD
   on PRD.product_name = ORD.baked_good_type;
+
+
+
+  -- create a dynamic table ORDERS_TBL in the DWH schema using the previous query
+create dynamic table ORDERS_TBL
+  target_lag = '1 minute'
+  warehouse = BAKERY_WH
+  as 
+select PT.partner_id, PRD.product_id, ORD.delivery_date, 
+  ORD.order_date, ORD.quantity  
+from STG.JSON_ORDERS_TBL_STG ORD
+inner join STG.PARTNER PT
+  on PT.partner_name = ORD.customer
+inner join STG.PRODUCT PRD
+  on PRD.product_name = ORD.baked_good_type;
